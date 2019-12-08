b965a9ea783c9dbaad786b688794056a643f31df
William Fecteau <william-fecteau@hotmail.com>
2019-12-08 14:22:07 -0500

    Day 7 and 8

---
+++ Day7/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day7
+{
+    class Program
+    {
+        const int END_CODE = 99;
+        const int ADD_CODE = 1;
+        const int MULTIPLY_CODE = 2;
+        const int INPUT_CODE = 3;
+        const int OUTPUT_CODE = 4;
+        const int JUMP_IF_TRUE_CODE = 5;
+        const int JUMP_IF_FALSE_CODE = 6;
+        const int LESS_THAN_CODE = 7;
+        const int EQUALS_CODE = 8;
+
+        static void Main(string[] args)
+        {
+            string[] program = ReadFile("./input.txt", ',');
+
+            int biggest = int.MinValue;
+
+            for (int one = 0; one < 5; one++)
+            {
+                for (int two = 0; two < 5; two++)
+                {
+                    for (int three = 0; three < 5; three++)
+                    {
+                        for (int four = 0; four < 5; four++)
+                        {
+                            for (int five = 0; five < 5; five++)
+                            {
+                                //This is trash but hey it works and I dont have enough time to optimize it
+                                if (one == two || one == three || one == four || one == five || two == three || two == four || two == five || three == four || three == five || four == five)
+                                {
+ 
+                                }
+                                else
+                                {
+                                    int output = RunComputer(program, five, RunComputer(program, four, RunComputer(program, three, RunComputer(program, two, RunComputer(program, one, 0)))));
+
+                                    if (output > biggest)
+                                    {
+                                        biggest = output;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Biggest: "+biggest);
+
+            //Order
+        }
+
+
+
+
+        public static int RunComputer(string[] program, int input1, int input2)
+        {
+            //Reading program
+            int[] intCodes = new int[program.Length];
+            for (int i = 0; i < program.Length; i++)
+            {
+                intCodes[i] = int.Parse(program[i]);
+            }
+
+            //Executing
+            int position = 0;
+            int output = 0;
+            while (intCodes[position] != END_CODE)
+            {
+                string code = intCodes[position].ToString();
+                int opCode;
+                int[] param = new int[3] { 0, 0, 0 };
+                if (code.Length < 2) opCode = int.Parse(code);
+                else
+                {
+                    opCode = int.Parse(code.Substring(code.Length - 2, 2));
+                    for (int i = 0; i < param.Length; i++)
+                    {
+                        try
+                        {
+                            param[i] = int.Parse(code.Substring(code.Length - 3 - i, 1));
+                        }
+                        catch (Exception e) { }
+                    }
+                }
+
+
+                int[] values = new int[3] { -1, -1, -1 };
+                for (int i = 0; i < values.Length; i++)
+                {
+                    //Position mode
+                    if (param[i] == 0)
+                    {
+                        values[i] = intCodes[position + 1 + i];
+                    }
+                    //Immediate mode
+                    else
+                    {
+                        values[i] = position + 1 + i;
+                    }
+                }
+
+
+                switch (opCode)
+                {
+                    case ADD_CODE:
+                        intCodes[values[2]] = intCodes[values[0]] + intCodes[values[1]];
+                        position += 4;
+                        break;
+                    case MULTIPLY_CODE:
+                        intCodes[values[2]] = intCodes[values[0]] * intCodes[values[1]];
+                        position += 4;
+                        break;
+                    case INPUT_CODE:
+                        //I just want it to work xd
+                        if(input1 != -69)
+                        {
+                            intCodes[values[0]] = input1;
+                            input1 = -69;
+                        }
+                        else
+                        {
+                            intCodes[values[0]] = input2;
+                        }
+                        position += 2;
+                        break;
+                    case OUTPUT_CODE:
+                        output = intCodes[values[0]];
+                        position += 2;
+                        break;
+                    case JUMP_IF_TRUE_CODE:
+                        if (intCodes[values[0]] != 0) position = intCodes[values[1]];
+                        else position += 3;
+                        break;
+                    case JUMP_IF_FALSE_CODE:
+                        if (intCodes[values[0]] == 0) position = intCodes[values[1]];
+                        else position += 3;
+                        break;
+                    case LESS_THAN_CODE:
+                        if (intCodes[values[0]] < intCodes[values[1]]) intCodes[values[2]] = 1;
+                        else intCodes[values[2]] = 0;
+                        position += 4;
+                        break;
+                    case EQUALS_CODE:
+                        if (intCodes[values[0]] == intCodes[values[1]]) intCodes[values[2]] = 1;
+                        else intCodes[values[2]] = 0;
+                        position += 4;
+                        break;
+                }
+            }
+
+            return output;
+        }
+
+        public static string[] ReadFile(string path, char separator)
+        {
+            System.IO.StreamReader reader = new System.IO.StreamReader(path);
+
+            string file = reader.ReadToEnd();
+
+            string[] lines = file.Split(separator);
+
+            return lines;
+        }
+    }
+}
---
+++ Day8/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+
+namespace Day8
+{
+    class Program
+    {
+        const int WIDTH = 25;
+        const int HEIGHT = 6;
+
+        const int BLACK = 0;
+        const int WHITE = 1;
+        const int TRANSPARENT = 2;
+
+        static void Main(string[] args)
+        {
+            string input = ReadFile("./input.txt");
+            int nbOfLayer = input.Length / (WIDTH * HEIGHT);
+
+            //Part 1
+            int smallestNbZero = int.MaxValue;
+            int theNbOfOne = 0;
+            int theNbOfTwo = 0;
+
+            //Part 2
+            int[,] finalImage = new int[HEIGHT, WIDTH];
+            for (int j = 0; j < HEIGHT; j++)
+            {
+                for (int k = 0; k < WIDTH; k++)
+                {
+                    //Initializing every value with transparent
+                    finalImage[j, k] = TRANSPARENT;
+                }
+            }
+
+
+            //Looping through every pixel of every layer
+            for (int i = 0; i < nbOfLayer; i++)
+            {
+                int nbOfZero = 0;
+                int nbOfOne = 0;
+                int nbOfTwo = 0;
+                for (int j = 0; j < HEIGHT; j++)
+                {
+                    for (int k = 0; k < WIDTH; k++)
+                    {
+                        int currentPixel = (int)Char.GetNumericValue(input[i * WIDTH * HEIGHT + j * WIDTH + k]);
+
+                        //Part 1
+                        switch (currentPixel)
+                        {
+                            case 0:
+                                nbOfZero++;
+                                break;
+                            case 1:
+                                nbOfOne++;
+                                break;
+                            case 2:
+                                nbOfTwo++;
+                                break;
+                        }
+
+                        //Part 2
+                        if (finalImage[j, k] == TRANSPARENT)
+                        {
+                            finalImage[j, k] = currentPixel;
+                        }
+                    }
+                }
+
+                //Checking for fewest number of zero
+                if (nbOfZero < smallestNbZero)
+                {
+                    smallestNbZero = nbOfZero;
+                    theNbOfOne = nbOfOne;
+                    theNbOfTwo = nbOfTwo;
+                }
+            }
+
+            //Printing for part 1
+            Console.WriteLine("=========Part 1=========");
+            Console.WriteLine("Number of 1 digits multiplied by the number of 2 digits: "+theNbOfOne*theNbOfTwo+"\n");
+
+            Console.WriteLine("=========Part 2=========");
+            //Printing for part 2
+            for (int j = 0; j < HEIGHT; j++)
+            {
+                for (int k = 0; k < WIDTH; k++)
+                {
+                    if (finalImage[j, k] == WHITE) Console.Write(finalImage[j, k]);
+                    else Console.Write(" ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public static string ReadFile(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+
+            string file = reader.ReadToEnd();
+
+            return file;
+        }
+    }
+}
